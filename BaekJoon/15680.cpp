9f30ab040204349468514696e2d0b2c81b40efda
Yebin Lee <50792985+SL313@users.noreply.github.com>
2022-06-29 14:45:13 +0900

    연세대학교

---
+++ BaekJoon/15680.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int N;
+	cin >> N;
+	if (N == 0) cout << "YONSEI" << endl;
+	else if(N == 1) cout << "Leading the Way to the Future" << endl;
+	return 0;
+}
