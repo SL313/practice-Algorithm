ebf39952a16951409b06a625d4d9be37066066a2
Yebin Lee <50792985+SL313@users.noreply.github.com>
2022-07-02 22:20:50 +0900

    Create K번째수.py

---
+++ Programmers/K번째수.py
@@ -0,0 +1,11 @@
+def solution(array, commands):
+    answer = []
+    for i in range(0, len(commands)):
+        newArray = []
+        range1=commands[i][0]
+        range2=commands[i][1]
+        num=commands[i][2]
+        newArray = list(array[range1-1:range2])
+        newArray.sort()
+        answer.append(newArray[num-1])
+    return answer
